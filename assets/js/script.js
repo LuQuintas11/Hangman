64c2e4a0eae19add804e673372f066dda585f583
LuQuintas11 <luciaquintas14@gmail.com>
2022-04-15 23:15:01 +0000

    add main function that check if the letter guessed is in the random word

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
    console.log("hola")
 });
 
-let words = ["deny", "hello", "dog", "cat"];
+let words = ["DENY", "HELLO", "DOG", "CAT"];
+
 let alphabet= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 let word = words[Math.floor(Math.random()*words.length )]
-
+console.log(word)
 // *this function will check the amount of letters and create the spaces*/
 function checkLetter(){
  for(let i=0; i<word.length;i++){
     let board = document.createElement('li');
-    board.textContent= "_";
+    board.textContent= "__";
+ 
     board.className = "blah";
     document.getElementById('main-div').appendChild(board);
  } 
@@ -35,31 +37,16 @@ createAlphabet();
 for(let i=0; i<alphabet.length;i++){
    let alpha = document.getElementsByClassName("alphabet")[i];
    alpha.addEventListener("click", function(event){
-      console.log(event.target)
+      event.target
       let alphabetValue = alphabet[i];
-      if(word.indexOf(alphabetValue)){
-         for(let i =0; i,word.length; i++){
-            if(word[i]===alphabetValue){
-               console.log("has ganado")
-            }
-         }
-      }
+   // console.log(alphabetValue);
+    if(word.includes(alphabetValue)){
+       for(let i =0; i<word.length; i++){
+          if(word[i]===alphabetValue){
+            document.getElementById("main-div").childNodes[i].textContent=alphabetValue
+          }
+       } 
+    }else{alert("your loose")}
+ 
    })
 }
-
-// .forEach(alphabet => {
-//     alphabet.addEventListener("click", function(event) {
-//         var alphabet = event.target;
-//         var alphabetValue = alphabet.textContent;
-
-//         if (word.contains(alphabetValue)) {
-//             for (var i=0; i<word.length; i++) {
-//                 if (word[i] == alphabetValue) {
-//                     document.getElementById("main-div").childNodes[i].textContent = alphabetValue;
-//                 }
-//             }
-//         } else {
-//             alphabet.disable = true;
-//         }
-//     })
-// });
