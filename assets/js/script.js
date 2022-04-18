746b15181d9d91ed93986ed3356316e35f3e6e3c
LuQuintas11 <luciaquintas14@gmail.com>
2022-04-18 20:03:02 +0000

    Add modal once the DOM is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
    console.log("DOM is loaded")
+ 
 });
+let modalBg = document.querySelector(".modal-bg");
+let modalClose = document.querySelector(".modal-close");
 
-let modalBtn = document.querySelector(".modal-btn");
-let modalBg = document.querySelector(".modal-bg")
-let modalClose = document.querySelector(".modal-close")
+ let modalBj = document.querySelector(".modal-bj");
+//  let modalClos = document.querySelector(".modal-clos");
+//  let modalBtn = document.querySelector(".modal-btn")
 let words = ["DENY", "HELLO", "DOG", "CAT"];
 let liveSpan=document.querySelector(".lives")
 let alphabet= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
@@ -12,8 +17,9 @@ let word= words[Math.floor(Math.random()*words.length )]
 console.log(word)
 // *this function will check the amount of letters and create the spaces*/
 
-
-
+if(document.readyState === "complete") {
+   modalBj.classList.add("bj-active")
+}
 
 
 modalClose.addEventListener("click", function(){
@@ -22,11 +28,10 @@ modalClose.addEventListener("click", function(){
 })
 
 function runGame(){
- 
-      modalBg.classList.add("bg-active")
-   
-
+  modalBg.classList.add("bg-active");
 }
+
+
 
 
 
@@ -34,7 +39,6 @@ function createSpaces(){
  for(let i=0; i<word.length;i++){
     let board = document.createElement('li');
     board.textContent= "__";
- 
     board.className = "blah";
     document.getElementById('main-div').appendChild(board);
  } 
@@ -69,6 +73,7 @@ for(let i=0; i<alphabet.length;i++){
        for(let i =0; i<word.length; i++){
           if(word[i]===alphabetValue){
             document.getElementById("main-div").childNodes[i].textContent=alphabetValue
+         
           }
        } 
     }else{decreaseLifes()}
@@ -78,15 +83,14 @@ for(let i=0; i<alphabet.length;i++){
 checkLetter()
 
 
-lives=5;
+lives=3;
 function decreaseLifes(){
    lives--;
    liveSpan.textContent = lives;
    if(lives===0){
      runGame() 
-   ilives=5;
+   lives=3;
    }
+  
 }
 
-
- 
