27d4739dbc4732aaa51f3e998bdab6c6d083e4b2
LuQuintas11 <luciaquintas14@gmail.com>
2022-04-23 21:46:43 +0000

    add only on game function instead of three

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,18 @@ let modalClose = document.querySelector(".modal-close");
 let modalBj = document.querySelector(".modal-bj");
 let animals = document.querySelector("#animals");
 
-let words = ["FOOTBALL", "BOXING", "CYCLING", "SWIMMING", "TENNIS", "CLIMBING", "BASKETBALL", "ATHLETICS", "BADMINTON"];
-let wordAnimal = ["CAT", "RABBIT", "LION", "ZEBRA", "HORSE", "TIGER", "BEAR", "FROG", "FROG", "MONKEY", "SNAKE"];
-let wordCinema = ["STAR WARS", "BATMAN", "THE KING", "JUMANJI", "SPIDERMAN", "VENOM", "FROZEN", "HARRY-POTTER"]
+let word = ["FOOTBALL", "BOXING", "CYCLING", "SWIMMING", "TENNIS", "CLIMBING", "BASKETBALL", "ATHLETICS", "BADMINTON"]
+
+let wordCinema =["STAR WARS", "BATMAN", "THE KING", "JUMANJI", "SPIDERMAN", "VENOM", "FROZEN", "HARRY-POTTER"]
+
 
 let pushAlphabet = [];
 
 let liveSpan = document.querySelector(".lives")
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ];
-let wordsSports = words[Math.floor(Math.random() * words.length)]
-let wordsAnimals = wordAnimal[Math.floor(Math.random()*wordAnimal.length)]
-let wordsCinema = wordCinema[Math.floor(Math.random()*wordCinema.length)]
-console.log(wordsCinema)
+// let wordsSports = word[Math.floor(Math.random() * word.length)]
+// let wordsAnimals = wordAnimal[Math.floor(Math.random()*wordAnimal.length)]
+// let wordsCinema = wordCinema[Math.floor(Math.random()*wordCinema.length)]
 
 
 function createAlphabet() {
@@ -38,8 +38,10 @@ document.addEventListener("DOMContentLoaded", function () {
       button.addEventListener("click", function () {
          if (this.getAttribute("data-type") === "animals") {
             modalBj.classList.remove("bj-active")
-            let wordsAnimals = wordAnimal[Math.floor(Math.random() * wordAnimal.length)]
-            animalGame(wordsAnimals)
+            let wordAnimal =["CAT", "RABBIT", "LION", "ZEBRA", "HORSE", "TIGER", "BEAR", "FROG", "FROG", "MONKEY", "SNAKE"]
+            let wordsAnimals = wordAnimal[Math.floor(Math.random()*wordAnimal.length)]
+            wordGuessed(wordsAnimals)
+            console.log(wordsAnimals)
          } else if (this.getAttribute("data-type") === "sports") {
             modalBj.classList.remove("bj-active")
             wordSport()
@@ -65,136 +67,172 @@ function runGame() {
 }
 
 
-
-function wordSport() {
-
-   for (let i = 0; i < wordsSports.length; i++) {
-      let board = document.createElement('li');
-      board.className = "blah";
-      document.getElementById('main-div').appendChild(board);
-      for(let word of wordsSports){
-         if(word===" "){
-            board.textContent=" ";
-         }else {
-            board.textContent = "_"
-         }
-      }
-   }
-   for (let i = 0; i < alphabet.length; i++) {
-      let alpha = document.getElementsByClassName("alphabet")[i];
-      alpha.addEventListener("click", function (event) {
-         event.target
-         let alphabetValue = alphabet[i];
-         if (wordsSports.includes(alphabetValue)) {
-            for (let i = 0; i < wordsSports.length; i++) {
-               if (wordsSports[i] === alphabetValue) {
-                document.getElementById("main-div").childNodes[i].textContent = alphabetValue  
-               pushAlphabet.push("alphabetValue")
-               if(wordsSports.length===pushAlphabet.length){
-                   console.log("hola")
-               }
-               }
+function wordGuessed(wor) {
+      for (let i = 0; i < wor.length; i++) {
+         let board = document.createElement('li');
+         board.className = "blah";
+         document.getElementById('main-div').appendChild(board);
+         for(let word of wor){
+            if(word===" "){
+               board.textContent=" ";
+            }else {
+               board.textContent = "_"
             }
-         } else {
-            decreaseLifes()
-         }
-
-      })
-   }
-}
-
-
-
-
-function animalGame() {
-   for (let i = 0; i < wordsAnimals.length; i++) {
-      let board = document.createElement('li');
-      board.className = "blah";
-      document.getElementById('main-div').appendChild(board);
-      for(let word of wordAnimal){
-         if(word===" "){
-            board.textContent= " ";
-         }else{
-            board.textContent="_"
          }
       }
-   
-   }
-
-   for (let i = 0; i < alphabet.length; i++) {
-      let alpha = document.getElementsByClassName("alphabet")[i];
-      alpha.addEventListener("click", function (event) {
-         event.target
-         let alphabetValue = alphabet[i];
-         if (wordsAnimals.includes(alphabetValue)) {
-            for (let i = 0; i < wordsAnimals.length; i++) {
-               if (wordsAnimals[i] === alphabetValue) {
-                  document.getElementById("main-div").childNodes[i].textContent = alphabetValue
+      for (let i = 0; i < alphabet.length; i++) {
+         let alpha = document.getElementsByClassName("alphabet")[i];
+         alpha.addEventListener("click", function (event) {
+            event.target
+            let alphabetValue = alphabet[i];
+            if (wor.includes(alphabetValue)) {
+               for (let i = 0; i < wor.length; i++) {
+                  if (wor[i] === alphabetValue) {
+                   document.getElementById("main-div").childNodes[i].textContent = alphabetValue  
                   pushAlphabet.push("alphabetValue")
-               if(wordsAnimals.length === pushAlphabet.length){
-                  alert("you won")
-               }
-               }
-            }
-         } else {
-            decreaseLifes()
-         }
-
-      })
-   }
-}
-
-function wordCinemas() {
-   for (let i = 0; i < wordsCinema.length; i++) {
-      let board = document.createElement('li');
-      // board.textContent = "_";
-      board.className = "blah"; 
-      document.getElementById('main-div').appendChild(board);
-   for(let word of wordCinema){
-      if(word===" "){
-         board.textContent= " ";
-      } else{
-         board.textContent="_"
-      }
-     
-   }
-      
-   }
-   for (let i = 0; i < alphabet.length; i++) {
-      let alpha = document.getElementsByClassName("alphabet")[i];
-      alpha.addEventListener("click", function (event) {
-         event.target
-         let alphabetValue = alphabet[i];
-         if (wordsCinema.includes(alphabetValue)) {
-            for (let i = 0; i < wordsCinema.length; i++) {
-               if (wordsCinema[i] === alphabetValue) {
-                  document.getElementById("main-div").childNodes[i].textContent = alphabetValue
-                  pushAlphabet.push("alphabetValue")
-                  if(wordsCinema.length===pushAlphabet.length){
+                  if(wor.length===pushAlphabet.length){
                       console.log("hola")
                   }
+                  }
                }
+            } else {
+               decreaseLifes()
             }
-         } else {
-            decreaseLifes()
-         }
-
-      })
-   }
+   
+         })
+      }
 }
+   
+
+// function wordSport() {
+
+//    for (let i = 0; i < wordsSports.length; i++) {
+//       let board = document.createElement('li');
+//       board.className = "blah";
+//       document.getElementById('main-div').appendChild(board);
+//       for(let word of wordsSports){
+//          if(word===" "){
+//             board.textContent=" ";
+//          }else {
+//             board.textContent = "_"
+//          }
+//       }
+//    }
+//    for (let i = 0; i < alphabet.length; i++) {
+//       let alpha = document.getElementsByClassName("alphabet")[i];
+//       alpha.addEventListener("click", function (event) {
+//          event.target
+//          let alphabetValue = alphabet[i];
+//          if (wordsSports.includes(alphabetValue)) {
+//             for (let i = 0; i < wordsSports.length; i++) {
+//                if (wordsSports[i] === alphabetValue) {
+//                 document.getElementById("main-div").childNodes[i].textContent = alphabetValue  
+//                pushAlphabet.push("alphabetValue")
+//                if(wordsSports.length===pushAlphabet.length){
+//                    console.log("hola")
+//                }
+//                }
+//             }
+//          } else {
+//             decreaseLifes()
+//          }
+
+//       })
+//    }
+// }
 
 
-lives = 3;
 
-function decreaseLifes() {
-   lives--;
-   liveSpan.textContent = lives;
-   if (lives === 0) {
-      lives = 3;
-      runGame()
-   }
 
-}
+// function animalGame() {
+//    for (let i = 0; i < wordsAnimals.length; i++) {
+//       let board = document.createElement('li');
+//       board.className = "blah";
+//       document.getElementById('main-div').appendChild(board);
+//       for(let word of wordAnimal){
+//          if(word===" "){
+//             board.textContent= " ";
+//          }else{
+//             board.textContent="_"
+//          }
+//       }
+   
+//    }
+
+//    for (let i = 0; i < alphabet.length; i++) {
+//       let alpha = document.getElementsByClassName("alphabet")[i];
+//       alpha.addEventListener("click", function (event) {
+//          event.target
+//          let alphabetValue = alphabet[i];
+//          if (wordsAnimals.includes(alphabetValue)) {
+//             for (let i = 0; i < wordsAnimals.length; i++) {
+//                if (wordsAnimals[i] === alphabetValue) {
+//                   document.getElementById("main-div").childNodes[i].textContent = alphabetValue
+//                   pushAlphabet.push("alphabetValue")
+//                if(wordsAnimals.length === pushAlphabet.length){
+//                   alert("you won")
+//                }
+//                }
+//             }
+//          } else {
+//             decreaseLifes()
+//          }
+
+//       })
+//    }
+// }
+
+// function wordCinemas() {
+//    for (let i = 0; i < wordsCinema.length; i++) {
+//       let board = document.createElement('li');
+//       // board.textContent = "_";
+//       board.className = "blah"; 
+//       document.getElementById('main-div').appendChild(board);
+//    for(let word of wordCinema){
+//       if(word===" "){
+//          board.textContent= " ";
+//       } else{
+//          board.textContent="_"
+//       }
+     
+//    }
+      
+//    }
+//    for (let i = 0; i < alphabet.length; i++) {
+//       let alpha = document.getElementsByClassName("alphabet")[i];
+//       alpha.addEventListener("click", function (event) {
+//          event.target
+//          let alphabetValue = alphabet[i];
+//          if (wordsCinema.includes(alphabetValue)) {
+//             for (let i = 0; i < wordsCinema.length; i++) {
+//                if (wordsCinema[i] === alphabetValue) {
+//                   document.getElementById("main-div").childNodes[i].textContent = alphabetValue
+//                   pushAlphabet.push("alphabetValue")
+//                   if(wordsCinema.length===pushAlphabet.length){
+//                       console.log("hola")
+//                   }
+//                }
+//             }
+//          } else {
+//             decreaseLifes()
+//          }
+
+//       })
+//    }
+// }
+
+
+// lives = 3;
+
+// function decreaseLifes() {
+//    lives--;
+//    liveSpan.textContent = lives;
+//    if (lives === 0) {
+//       lives = 3;
+//       runGame()
+//    }
+
+// }
 
 
 
